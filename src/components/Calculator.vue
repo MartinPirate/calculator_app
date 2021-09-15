b00ad01adb304c6f423f7bce4dbf9119b6130162
martin kiragu <martin@gratitudeindia.com>
2021-09-15 11:32:55 +0300

    simple logic for a calculator

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,26 +1,26 @@
 <template>
   <div class="calculator">
 
-    <div class="display">0.000668</div>
-    <div class="btn">C</div>
-    <div class="btn ">+/-</div>
-    <div class="btn ">%</div>
-    <div class="btn operator">/</div>
-    <div class="btn">7</div>
-    <div class="btn">8</div>
-    <div class="btn">9</div>
-    <div class="btn operator">X</div>
-    <div class="btn">4</div>
-    <div class="btn">5</div>
-    <div class="btn">6</div>
-    <div class="btn operator">-</div>
-    <div class="btn">1</div>
-    <div class="btn">2</div>
-    <div class="btn">3</div>
-    <div class="btn operator ">+</div>
-    <div class="btn zero">0</div>
-    <div class="btn ">.</div>
-    <div class="btn operator">=</div>
+    <div class="display">{{ current || 0 }}</div>
+    <div @click="clear" class="btn">C</div>
+    <div @click="sign" class="btn">+/-</div>
+    <div @click="percent" class="btn ">%</div>
+    <div @click="divide" class="btn operator">/</div>
+    <div @click="append('7')" class="btn">7</div>
+    <div @click="append('8')" class="btn">8</div>
+    <div @click="append('9')" class="btn">9</div>
+    <div @click="multiply" class="btn operator">X</div>
+    <div @click="append('4')" class="btn">4</div>
+    <div @click="append('5')" class="btn">5</div>
+    <div @click="append('6')" class="btn">6</div>
+    <div @click="subtract" class="btn operator">-</div>
+    <div @click="append('1')" class="btn">1</div>
+    <div @click="append('2')" class="btn">2</div>
+    <div @click="append('3')" class="btn">3</div>
+    <div @click="add" class="btn operator ">+</div>
+    <div @click="append('0')" class="btn zero">0</div>
+    <div @click="dot" class="btn ">.</div>
+    <div @click="equal" class="btn operator">=</div>
 
   </div>
 
@@ -28,9 +28,68 @@
 
 <script>
 export default {
-  name: 'Calculator',
-  props: {
-    msg: String
+
+  data() {
+    return {
+      previous: null,
+      current: '',
+      operator: null,
+      operatorClicked: false
+    }
+  },
+  methods: {
+    clear() {
+      this.current = ''
+    },
+    sign() {
+      //append a minus or remove ternally operator
+      this.current = this.current.charAt(0) === '-' ?
+          this.current.slice(1) : `-${this.current}`
+    },
+    percent() {
+      this.current = `${parseFloat(this.current) / 100}`
+    },
+
+    append(number) {
+      if (this.operatorClicked) {
+        this.current = '';
+        this.operatorClicked = false
+      }
+      this.current = `${this.current}${number}`
+    },
+    dot() {
+      if (this.current.indexOf('.') === -1) {
+        this.append('.')
+      }
+
+    },
+    setPrevious() {
+      this.previous = this.current;
+      this.operatorClicked = true;
+
+    },
+
+    divide() {
+      this.operator = (a, b) => a / b;
+      this.setPrevious();
+
+    },
+    multiply() {
+      this.operator = (a, b) => a * b;
+      this.setPrevious();
+    },
+    subtract() {
+      this.operator = (a, b) => a - b;
+      this.setPrevious();
+    },
+    add() {
+      this.operator = (a, b) => a + b;
+      this.setPrevious();
+    },
+    equal() {
+      this.current = `${this.operator(parseFloat(this.previous),parseFloat(this.current))}`
+      this.previous = null;
+    }
   }
 }
 </script>
@@ -38,33 +97,37 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
-.calculator{
+.calculator {
   margin: 0 auto;
   width: 400px;
   font-size: 40px;
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-auto-rows: minmax(50px, auto);
+  -moz-box-shadow:    3px 3px 5px 6px #ccc;
+  -webkit-box-shadow: 3px 3px 5px 6px #ccc;
+  box-shadow:         3px 3px 5px 6px #ccc;
 }
 
-.display{
+.display {
   /*specify how many column the display takes*/
-  grid-column:1/5 ;  /*start at column one and end after column 4*/
+  grid-column: 1/5; /*start at column one and end after column 4*/
   background-color: #333333;
   color: white;
 }
-.btn{
+
+.btn {
   background-color: #F2F2F2;
   border: 1px solid #999;
 }
 
-.operator{
+.operator {
   background-color: orange;
   color: #eeeeee;
 }
 
-.zero{
-  grid-column:1/3 ;
+.zero {
+  grid-column: 1/3;
 }
 
 
