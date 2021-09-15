38453903e0cd50ea868faaa28d3c58b1849009f8
martin kiragu <martin@gratitudeindia.com>
2021-09-15 10:35:21 +0300

    4* infinity grid system style css, with a height of 50px

---
+++ src/components/Calculator.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="calculator">
+
+    <div>Hello</div>
+    <div>Hello</div>
+    <div>Hello</div>
+    <div>Hello</div>
+    <div>Hello</div>
+    <div>Hello</div>
+    <div>Hello</div>
+    <div>Hello</div>
+    <div>Hello</div>
+    <div>Hello</div>
+    <div>Hello</div>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'Calculator',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.calculator{
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-rows: minmax(50px, auto);
+}
+
+</style>
